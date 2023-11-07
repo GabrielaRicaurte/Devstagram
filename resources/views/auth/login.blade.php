7e5877cb7952f65e916bbd7ae39a217f9415341e
GabrielaXd2004 <gabyruizricaurte2004@gmail.com>
2023-11-07 09:18:27 -0400

    revisar si un usuario esta autenticado o no

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Inicia Sesión en DevStagram
+@endsection
+
+@section('contenido')
+    <div class="md:flex md:justify-center md:gap-10 md:items-center">
+        <div class="md:w-6/12 p-5">
+            <img src="{{ asset('asssets/img/login.jpg') }}" alt="imagen login de usuario" class="">
+        </div>
+        <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
+            <form action="{{ route('login') }}" method="POST" novalidate>
+                @csrf
+
+                @if (session('mensaje'))
+                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ session('mensaje') }}</p>
+                @endif
+
+                <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">email</label>
+                    <input type="email" name="email" id="email" placeholder="Tu Email de Registro"
+                        value="{{ old('email') }}"
+                        class="border p-3 w-full rounded-lg  @error('email') border-red-500 @enderror">
+                    @error('email')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-5">
+                    <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">password</label>
+                    <input type="password" name="password" id="password" placeholder="Tu Password de Registro"
+                        class="border p-3 w-full rounded-lg  @error('password') border-red-500 @enderror">
+                    @error('password')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <input type="submit" value="Crear Cuenta"
+                    class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
+            </form>
+        </div>
+    </div>
+@endsection
